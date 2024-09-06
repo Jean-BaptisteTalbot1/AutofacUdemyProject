decb6de8c255729b4cbd72664915d4b5f7847be0
Jean-BaptisteTalbot1 <jean-baptiste.talbot@outlook.com>
2024-09-06 10:59:29 -0400

    complete code of all the chapters on the section 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -72,16 +72,53 @@ namespace ConsoleApp1
         {
             var builder = new ContainerBuilder();
 
+            // Whenever someone asks for a ILog, give them a ConsoleLog. So if Engine or Car asks for a ILog, it will get a ConsoleLog.
+            // On the following line, if we only want to give a simple ConsoleLog to any ILog :
+            builder.RegisterType<ConsoleLog>().As<ILog>();
+            // But here, is a way to specify the instance, so every time someone asks for a ILog, they will get the same instance of
+            // ConsoleLog.This is useful when we want to share the same instance of a type across the application.
+            // It prevents to create a new instance of ConsoleLog every time someone asks for a ILog.
+            var log = new ConsoleLog();
+            builder.RegisterInstance(log).As<ILog>();
+
+            builder.Register(c =>
+                new Engine(c.Resolve<ILog>(), 123));
+            //builder.RegisterType<Engine>();
+
+            // Here we have two constructors for Car. The first one is the default one, and the second one is the one that takes an Engine
+            // and a ILog.
+            builder.RegisterType<Car>();
+            // The UsingConstructor() method is used to specify which constructor to use when creating an instance of a type.
+            // If we have multiple constructors, we can use the UsingConstructor() method to specify which one to use.
+            // If we need to use a specific constructor, we can pass the types of the parameters of that constructor to the UsingConstructor() method
+            // to specify which constructor to use by writing explicitly the types of the parameters of that constructor like this:
+            // builder.RegisterType<Car>().UsingConstructor(typeof(Engine)); -> We take the public Car(Engine engine) signature.
+            // If we don't specify the constructor, the most complex one will be used.
+
+
             // IList<T> --> List<T>
             // IList<int> --> List<int>
-
-            // With this, we can add a IList on a constructor and it will be resolved to a List (of a generic type, so anything)
+            // With this, we can add a IList on a constructor and, it will be resolved to a List (of a generic type, so anything)
             builder.RegisterGeneric(typeof(List<>)).As(typeof(IList<>));
 
+            // Having registered all those types on the builder, the builder can now be used to construct the container.
             IContainer container = builder.Build();
 
             var myList = container.Resolve<IList<int>>();
             Console.WriteLine(myList.GetType().Name);
+
+            // So this container can be used to resolve types and instantiate them.
+            // Then to get an instance of Car, we can ask the container to resolve it.  
+            var car = container.Resolve<Car>();
+
+            // Once we have registered the ConsoleLog as a ILog, the container will automatically inject it into the Engine and Car.
+            // ALso, the consoleLog will not be accessible to the resolvers anymore because we have registered it as a ILog.
+            // ***************
+            //// That is to say, that something like that will not work: var log = container.Resolve<ConsoleLog>();
+            //// But if we want to register a ConsoleLog as well, we can do it like this: builder.RegisterType<ConsoleLog>().AsSelf(); and then the Resole<ConsoleLog>() will work.
+            // ***************
+
+            car.Go();
         }
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051592b78afae739b11777684fdf2f8c563c0635")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aff5c1bdeda2121b56bad1f894f7d2f184c9314")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
