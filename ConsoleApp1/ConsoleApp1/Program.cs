1e9586cab3a6a133fa854cf696d8143a94ea2682
Jean-BaptisteTalbot1 <jean-baptiste.talbot@outlook.com>
2024-09-06 16:43:17 -0400

    Section4 - Chapter23 - Delayed Instantiation

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,11 @@ namespace ConsoleApp1
 
     public class ConsoleLog : ILog
     {
+        public ConsoleLog()
+        {
+            Console.WriteLine($"{nameof(ConsoleLog)} instance created at {DateTime.Now.Ticks}");
+        }
+
         public void Write(string message)
         {
             Console.WriteLine(message);
@@ -19,6 +24,7 @@ namespace ConsoleApp1
 
         public void Dispose()
         {
+            Console.WriteLine("ConsoleLog instance disposed");
         }
     }
 
@@ -41,12 +47,39 @@ namespace ConsoleApp1
         }
     }
 
+    public class Reporting
+    {
+        private readonly Lazy<ConsoleLog> log;
+
+        public Reporting(Lazy<ConsoleLog> log)
+        {
+            if (log == null)
+            {
+                throw new ArgumentNullException(nameof(log));
+            }
+            this.log = log;
+            Console.WriteLine($"{nameof(Reporting)} instance created at {DateTime.Now.Ticks}");
+        }
+
+        public void Report()
+        {
+            log.Value.Write("Log started");
+        }
+    }
+
     internal class Program
     {
         public static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
 
+            builder.RegisterType<ConsoleLog>();
+            builder.RegisterType<Reporting>();
+
+            using (var container = builder.Build())
+            {
+                container.Resolve<Reporting>().Report();
+            }
         }
     }
 }
