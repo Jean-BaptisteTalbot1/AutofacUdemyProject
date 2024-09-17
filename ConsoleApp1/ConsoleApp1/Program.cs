2d4a3a0e15ebe59e55cb90563ae7ffb0e8dddcdc
Jean-BaptisteTalbot1 <jean-baptiste.talbot@outlook.com>
2024-09-17 11:31:18 -0400

    Section5 - Chapter 35 - Disposal

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,37 +7,132 @@ using Module = Autofac.Module;
 
 namespace AutofacSamples
 {
-    public class IResource
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public interface IConsole
     {
 
     }
 
-    class SingletonResource : IResource
+    public class ConsoleLog : ILog, IConsole, IDisposable
     {
-    }
-
-    public class InstancePerDependencyResource: IResource, IDisposable
-    {
-        public InstancePerDependencyResource()
+        public ConsoleLog()
         {
-            Console.WriteLine("Instance per dep created");
+            Console.WriteLine("Creating a console log!");
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
         }
 
         public void Dispose()
         {
-            Console.WriteLine("Instance per dep disposed");
+            Console.WriteLine("Console log is being disposed!");
         }
     }
 
-    public class ResourceManager
+    public class EmailLog : ILog
     {
-        public ResourceManager(IEnumerable<IResource> resources)
+        private const string adminEmail = "admin@foo.com";
+
+        public void Write(string message)
         {
-            Resources = resources ?? throw new ArgumentNullException(nameof(resources));
-            Console.WriteLine("ResourceManager.ctor");
+            Console.WriteLine($"Email sent to {adminEmail} : {message}");
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            id = new Random().Next();
         }
 
-        public IEnumerable<IResource> Resources { get; set; }
+        public Engine(ILog log, int id)
+        {
+            this.log = log;
+            this.id = id;
+        }
+
+        public void Ahead(int power)
+        {
+            log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+    public class SMSLog : ILog
+    {
+        private string phoneNumber;
+
+        public SMSLog(string phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine($"SMS to {phoneNumber} : {message}");
+        }
+    }
+
+    public class Car
+    {
+        private Engine engine;
+        private ILog log;
+
+        public Car(Engine engine)
+        {
+            this.engine = engine;
+            this.log = new EmailLog();
+        }
+
+        public Car(Engine engine, ILog log)
+        {
+            this.engine = engine;
+            this.log = log;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(100);
+            log.Write("Car going forward...");
+        }
+    }
+
+    public class Parent
+    {
+        public override string ToString()
+        {
+            return "I am your father";
+        }
+    }
+
+    public class Child
+    {
+        public string Name { get; set; }
+        public Parent Parent { get; set; }
+
+        public void SetParent(Parent parent)
+        {
+            Parent = parent;
+        }
+    }
+
+    public class ParentChildModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Parent>();
+            builder.Register(c => new Child() { Parent = c.Resolve<Parent>() });
+        }
     }
 
     internal class Program
@@ -45,20 +140,16 @@ namespace AutofacSamples
         public static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<ResourceManager>().SingleInstance();
-            builder.RegisterType<SingletonResource>()
-                .As<IResource>().SingleInstance();
-            builder.RegisterType<InstancePerDependencyResource>()
-                .As<IResource>();
+            //builder.RegisterType<ConsoleLog>();
+            builder.RegisterInstance(new ConsoleLog());
+            var container = builder.Build();
 
-            using (var container = builder.Build())
+            using (var scope = container.BeginLifetimeScope())
             {
-                using (var scope = container.BeginLifetimeScope())
-                {
-                    var resourceManager = scope.Resolve<ResourceManager>();
-                    Console.WriteLine("ResourceManager.Resources.Count: " + resourceManager.Resources.Count());
-                }
+                var log = scope.Resolve<ConsoleLog>();
+                log.Write("Hello from console log!");
             }
+            
         }
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c710c2afa26b7fcbe0ffed341fa54852b7a982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71d5f8eab51783c9a6c9b989b00edda0e59c7c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
