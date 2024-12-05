2e89865af7ad9d23385de003d59c28cd6feaf862
Jean-BaptisteTalbot1 <jean-baptiste.talbot@outlook.com>
2024-12-05 11:17:52 -0500

    Section6 - Module41 - Using Modules

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,11 +7,77 @@ using Module = Autofac.Module;
 
 namespace AutofacSamples
 {
+    public interface IVehicle
+    {
+        void Go();
+    }
+
+    class Truck : IVehicle
+    {
+        IDriver driver;
+
+        public Truck(IDriver driver)
+        {
+            if (driver == null)
+            {
+                throw new ArgumentNullException(nameof(driver));
+            }
+            this.driver = driver;
+        }
+
+        public void Go()
+        {
+            driver.Drive();
+        }
+    }
+
+    public interface IDriver
+    {
+        void Drive();
+    }
+
+    public class CrazyDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Going to fast and crashing into a tree");
+        }
+    }
+
+    public class SaneDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving safely to destination");
+        }
+    }
+
+    public class TransportModule : Module
+    {
+        public bool ObeySpeedLimit { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (ObeySpeedLimit)
+                builder.RegisterType<SaneDriver>().As<IDriver>();
+            else
+                builder.RegisterType<CrazyDriver>().As<IDriver>();
+
+            builder.RegisterType<Truck>().As<IVehicle>();
+        }
+    }
+
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TransportModule{ObeySpeedLimit = true});
 
+            using (var c = builder.Build())
+            {
+                c.Resolve<IVehicle>().Go();
+            }
         }
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2797d68857e69f37cedc30ea3543a72408d1a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2dc66730359376527f09c003a3f9c525f49ff22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
