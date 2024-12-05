09aa3e241cf732b9d81a559d870546477018ce32
Jean-BaptisteTalbot1 <jean-baptiste.talbot@outlook.com>
2024-12-05 11:20:15 -0500

    empty to prepare the module 42 of the section 6

--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2dc66730359376527f09c003a3f9c525f49ff22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e89865af7ad9d23385de003d59c28cd6feaf862")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
